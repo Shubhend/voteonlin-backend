b6f4a61d5260c89d24e3a04b86cd5b2d4078e58d
xuark <admin@xuark.com>
2021-08-17 21:20:49 +0530

    add

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,19 @@
 const express = require('express'); //import express
 
-
+var session = require('express-session')
 const router  = express.Router();
 // 2.
 const teaController = require('../controlers/AdminController');
 // 3.
 
 
-router.get('/', teaController.newTea);
+router.get('/admin', teaController.newTea);
 
 router.get('/login', teaController.login);
 
 router.post('/login', teaController.login);
+
+router.post('/admin/logout', teaController.login);
 
 // 4.
 module.exports = router; // export to use in server.js
